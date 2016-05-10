bd5a02c980eab6cbf679274ff61f2479e0e084c0
Jonathan Alexander <jonathandalexander@gmail.com>
2016-05-10 17:28:23 -0400

    Done. Added ability to allow multiple hits.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,18 +52,11 @@ def runner
   welcome
   card_total = initial_round
 
-  if card_total == 21
-    puts "Blackjack!"
-  elsif card_total < 21
+  while card_total < 21
     card_total = hit?(card_total)
     display_card_total(card_total)
-
-    if card_total == 21
-      puts "Blackjack!"  
-    elsif card_total > 21
-      end_game(card_total)
-    end
-  else card_total > 21
-    end_game(card_total)
   end
+
+  "Blackjack!" if card_total == 21
+  end_game(card_total) if card_total > 21
 end
